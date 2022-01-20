953e4f136c74f6ed72405feae34e7a32fb7a4bba
JFMajer <jakubmajer@live.com>
2022-01-20 20:16:02 +0100

    added route table

--- main.tf
+++ main.tf
@@ -25,3 +25,22 @@ resource "aws_internet_gateway" "dev_igw" {
     Name = "${var.name_prefix}-igw"
   }
 }
+
+resource "aws_route_table" "dev_public_route_table" {
+  vpc_id = aws_vpc.dev_vpc.id
+
+  tags = {
+    Name = "${var.name_prefix}-public-route-table"
+  }
+}
+
+resource "aws_route" "dev_public_route" {
+  route_table_id         = aws_route_table.dev_public_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.dev_igw.id
+}
+
+resource "aws_route_table_association" "dev_public_route_table_association" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.dev_public_route_table.id
+}
